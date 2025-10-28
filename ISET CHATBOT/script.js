427703847202faabd19359994495fa37599a407f
Mel <melgmoroni@gmail.com>
2025-10-28 16:12:31 -0300

    Add files via upload

---
+++ ISET CHATBOT/script.js
@@ -0,0 +1,5 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Aquí harías validación o conexión a servidor
+  window.location.href = "menu.html";
+});
